1dd6dda231920d3235d1fc590cf188341646d733
abdulkalam556 <shaikabdulkalamazad1@gmail.com>
2025-02-21 00:33:59 -0500

    graceful server shutdown, testing user endpoints

---
+++ cmd/api/test_utils.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	//"github.com/MishNia/Sportify/internal/auth"
+	"github.com/MishNia/Sportify/internal/store"
+	"github.com/MishNia/Sportify/internal/store/cache"
+	"go.uber.org/zap"
+)
+
+func newTestApplication(t *testing.T, cfg config) *application {
+	t.Helper()
+
+	logger := zap.NewNop().Sugar()
+	// Uncomment to enable logs
+	// logger := zap.Must(zap.NewProduction()).Sugar()
+	mockStore := store.NewMockStore()
+	mockCacheStore := cache.NewMockStore()
+
+	//testAuth := &auth.TestAuthenticator{}
+
+	// Rate limiter
+	// rateLimiter := ratelimiter.NewFixedWindowLimiter(
+	// 	cfg.rateLimiter.RequestsPerTimeFrame,
+	// 	cfg.rateLimiter.TimeFrame,
+	// )
+
+	return &application{
+		logger:        logger,
+		store:         mockStore,
+		cacheStorage:  mockCacheStore,
+		config:        cfg,
+		//authenticator: testAuth,
+		//rateLimiter:   rateLimiter,
+	}
+}
+
+func executeRequest(req *http.Request, mux http.Handler) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	mux.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d", expected, actual)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/go-playground/validator/v10 v10.24.0
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.10.0
 	github.com/swaggo/http-swagger v1.3.4
 	github.com/swaggo/swag v1.16.4
 	go.uber.org/zap v1.27.0
@@ -16,8 +17,11 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 )
 
 require (
